a99ed92b891be9bc7ea86fae44dcdc42e4e2e35d
Douglas Fleischut <df@douglass-imac.lan>
2018-01-26 21:51:52 -0800

    opacity

--- sketch.js
+++ sketch.js
@@ -5,16 +5,16 @@ let opacity =255;
 let angle = 0;
 let inc = 0.05;
 let circles = [];
-let numCircles = 400;
+let numCircles = 600;
 let osc =1;
 // let scroll=0.1;
 
 function setup() {
     createCanvas(1500,800);
-     frameRate(5);
+     frameRate(10);
  
         for(let i = 0; i < numCircles; i++) {
-        circles[i] = new Circle( i*3+100,100,size);
+        circles[i] = new Circle( i*3-100,-100,size);
        
     }
 
@@ -28,7 +28,9 @@ function draw() {
   
     background(random(130));
   
-    
+    scale(0.9);
+    //translate(500,500);
+   // rotate(angle/100);
         for(let i = 0; i < numCircles; i++) {
      
         osc = map(sin(angle),-1,1,0,height);  
@@ -55,7 +57,7 @@ let Circle = function(x, y, size) {
     this.x = x;
     this.y = y;
     this.size = size;
-    this.color = [random(255),random(255),random(255)];
+    this.color = [random(255),0,random(255),random(255)];
     
     this.display = function() {
         fill(this.color);
@@ -66,7 +68,7 @@ let Circle = function(x, y, size) {
 //        console.log(mult);
         console.log(this.y);
          // this.x = this.x+0.1;
-       let sizeOsc = map(tan(angle),-1,1,0,100);
+       let sizeOsc = map(tan(angle/2)*sin(osc),-1,1,0,100);
         this.size = sizeOsc/10;
         this.y = this.y/8+osc/2+100;
   
